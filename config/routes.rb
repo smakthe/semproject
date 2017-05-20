e16705ef89c79f87b8aa3ac5e24fb821953dd6ea
shayeridas <user.shayeri@outlook.com>
2017-05-21 00:54:22 +0530

    set devise authentication login page as root for unauthenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'students#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root 'students#index'
+  
   get 'videos/index' => 'students#video_index'
   get 'canvas/index' => 'students#canvas_index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20170519063719) do
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
